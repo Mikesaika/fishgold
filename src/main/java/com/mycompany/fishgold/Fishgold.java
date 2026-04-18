07ad6e9adff16b50b13158a54441e1f00b61b479
migue <migue@Mike>
2026-04-18 16:38:24 -0500

    Netbeans desde 0

---
+++ src/main/java/com/mycompany/fishgold/Fishgold.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fishgold;
+
+/**
+ *
+ * @author migue
+ */
+public class Fishgold {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
